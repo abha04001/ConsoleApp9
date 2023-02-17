fc272d76b46cfeb467080ee68187c4a97a7b0d40
Pingvu <nhlpool27@gmail.com>
2023-02-17 14:57:55 +0100

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ange bredden på rektangeln: ");
+        int bredd = int.Parse(Console.ReadLine());
+
+        Console.Write("Ange höjden på rektangeln: ");
+        int höjd = int.Parse(Console.ReadLine());
+
+        int area = AREA(bredd, höjd);
+        Console.WriteLine("Rektangelns area är: " + area);
+    }
+
+    static int AREA(int bredd, int höjd)
+    {
+        return bredd * höjd;
+    }
+}
